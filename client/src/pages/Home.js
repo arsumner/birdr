e3b3ebdc50b0005e89433f58becf79fd9f8169ce
Amanda Sumner <sumnera@oregonstate.edu>
2024-07-13 15:01:55 -0700

    created Home function to fetch & list birds

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useState, useEffect} from 'react'
+
 const Home = () => {
+    const [birds, setBirds] = useState(null)
+
+    useEffect(() => {
+        const fetchBirds = async () => {
+            const response = await fetch('api/birds')
+            const json = await response.json()
+
+            if (response.ok) {
+                setBirds(json)
+            }
+        }
+
+        fetchBirds()
+    }, [])
+
     return (
         <div className= "home">
-            <h2>Home</h2>
+            <div className="birds">
+                {birds && birds.map((bird) => (
+                    <p key={birds._id}>{bird.name}</p>
+                ))
+                }
+            </div>
         </div>
     )
 }
