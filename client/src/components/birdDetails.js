8964b7bcda954e910ed28f064ee33af3bbb46847
Amanda Sumner <sumnera@oregonstate.edu>
2024-07-14 11:24:29 -0700

    created component for bird details

--- client/src/components/birdDetails.js
+++ client/src/components/birdDetails.js
@@ -1,13 +1,39 @@
-const birdDetails = ({bird}) => {
+import { useBirdsContext } from "../hooks/useBirdsContext"
+import { useNavigate } from 'react-router-dom'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const BirdDetails = ({bird}) => {
+
+    const { dispatch } = useBirdsContext()
+    const navigate = useNavigate()
+
+    const handleDeleteClick = async () => {
+        const response = await fetch('/api/birds/' + bird._id, {
+            method: "DELETE"
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type: 'DELETE_BIRD', payload: json})
+        }
+    }
+
+    const handleUpdateClick = async () => {
+        navigate(`/update/${bird._id}`)
+    }
+    
+
     return (
         <div className="bird-details">
             <h4>{bird.name}</h4>
             <p>Location: {bird.location}</p>
             <p>Number of birds seen: {bird.count}</p>
             <p>Notes: {bird.notes}</p>
-            <p>{bird.createdAt}</p>
+            <p>{'Entry added: ' + formatDistanceToNow(new Date(bird.createdAt), { addSuffix: true})}</p>
+            <button onClick={handleDeleteClick}>delete</button>
+            <button onClick={handleUpdateClick}>update</button>
         </div>
     )
 }
 
-export default birdDetails
+export default BirdDetails
