208d357aaacf55c2bcb2f571de1aa1cc15474d9d
Amanda Sumner <sumnera@oregonstate.edu>
2024-07-18 12:45:28 -0700

    Added auth context with reducer for login/out

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BirdContextProvider } from './context/BirdContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BirdContextProvider>
-    <App />
-    </BirdContextProvider>
+    <AuthContextProvider>
+      <BirdContextProvider>
+        <App />
+      </BirdContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
