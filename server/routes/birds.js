a11c342c28d00954e1f011f96afdcc7c0ca9da4e
Amanda Sumner <sumnera@oregonstate.edu>
2024-07-18 18:45:30 -0700

    added middleware to protect API routes

--- server/routes/birds.js
+++ server/routes/birds.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const Bird = require('../models/birdModel')
 const { createBird, getBird, getBirds, deleteBird, updateBird } = require('../controllers/birdController')
+const requireAuth = require('../middleware/requireAuth')
 const router = express.Router()
+
+// middleware to require authentication to protect other routes
+router.use(requireAuth)
 
 // GET birds
 router.get('/', getBirds)
