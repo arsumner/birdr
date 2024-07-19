fa0f0c2c939642f52dd2e363aa0d9d1391d42838
Amanda Sumner <sumnera@oregonstate.edu>
2024-07-19 15:09:25 -0700

    added console log statements for debugging

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -2,26 +2,30 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
 const requireAuth = async (req, res, next) => {
-
-    // verification for authentication
-    const { authorization } = req.headers 
+    const { authorization } = req.headers
 
     if (!authorization) {
-        return res.status(401).json({error: 'Authorization token reqeuired.'})
+        console.log('Authorization header missing')
+        return res.status(401).json({ error: 'Authorization token required.' })
     }
+
+    console.log('Authorization header received:', authorization)
 
     // split to get the auth token from the authentication string
     const token = authorization.split(' ')[1]
+    console.log('Extracted token:', token)
 
     try {
-        const {_id} = jwt.verify(token, process.env.SECRET)
+        const { _id } = jwt.verify(token, process.env.SECRET)
+        console.log('Token verified, user ID:', _id)
 
         req.user = await User.findOne({ _id }).select('_id')
-        next()
+        console.log('User found:', req.user)
 
+        next()
     } catch (error) {
-        console.log(error)
-        res.status(401).json({error: 'Request is not authorized.'})
+        console.log('Error verifying token:', error)
+        res.status(401).json({ error: 'Request is not authorized.' })
     }
 }
 
