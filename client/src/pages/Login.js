ec91c110a0b59fe2b63bb60991c847a263e2b8ce
Amanda Sumner <sumnera@oregonstate.edu>
2024-08-04 13:45:14 -0700

    improved formatting of login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useLogin } from '../hooks/useLogin'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     const [email, setEmail] = useState('')
@@ -13,25 +14,33 @@ const Login = () => {
     }
 
     return (
+        
+        <div className="background-container">
         <form className="login" onSubmit={handleSubmit}>
-            <h3>Log in</h3>
-
-            <label>Email: </label>
+            <h3>Welcome back!</h3>
+            <h4>Sign in to Birdr</h4>
+            <div className='login-form-labels'>
+            <label>Email </label>
             <input
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
             />
-            <label>Password: </label>
+            <label>Password </label>
             <input
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
             />
+            </div>
 
-            <button disabled={isLoading}>Log in</button>
+            <button disabled={isLoading}>Submit</button>
             {error && <div className="error">{error}</div>}
         </form>
+        <div className="login-link">
+                <h4>Don't have a Birdr account? <Link to="/signup">Create an account</Link>.</h4>
+            </div>
+        </div>
     )
 }
 
