ab413ed401d32d1c2acb613a1bc515a2ee95ad6b
Amanda Sumner <sumnera@oregonstate.edu>
2024-08-04 17:03:40 -0700

    added auth header to only allow auth users

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -1,5 +1,6 @@
 import { useEffect} from 'react'
 import { useBirdsContext } from '../hooks/useBirdsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // components
 import BirdDetails from '../components/birdDetails'
@@ -7,10 +8,15 @@ import BirdForm from '../components/birdForm'
 
 const Library = () => {
     const {birds, dispatch} = useBirdsContext()
+    const {user} = useAuthContext()
 
     useEffect(() => {
         const fetchBirds = async () => {
-            const response = await fetch('api/birds')
+            const response = await fetch('api/birds', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
             const json = await response.json()
 
             if (response.ok) {
@@ -18,18 +24,20 @@ const Library = () => {
             }
         }
 
-        fetchBirds()
-    }, [])
+        if (user) {
+            fetchBirds()
+        }
+    }, [dispatch, user])
 
     return (
-        <div className= "library">
+        <div className= "">
+            <BirdForm/>
             <div className="birds">
                 {birds && birds.map((bird) => (
                     <BirdDetails key={bird._id} bird={bird}/>
                 ))
                 }
             </div>
-            <BirdForm/>
         </div>
     )
 }
