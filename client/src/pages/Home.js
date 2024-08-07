6b7fbe1e00463cbaf314a02714af11cd70ef8d5c
Amanda Sumner <sumnera@oregonstate.edu>
2024-08-07 16:49:04 -0700

    improved styling on Home page with links

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import note from '../images/note.png'; // Adjust the path as needed
+import React from 'react'
+import { Link } from 'react-router-dom'
+import treeOne from '../images/tree1.png'
+import treeTwo from '../images/tree2.png'
+
 
 const Home = () => {
     return (
         <div className="home">
-            <div className="hero">
-                <div>
-                    <h1>Welcome to your Birdr account!</h1>
-                    <p>Add and update your bird log in the library.</p>
-                </div>
-            </div>
-
             <div className="image-container">
-                <div className="image-wrapper">
-                    <img
-                        src={note}
-                        alt="Bird Image 1"
-                        className="large-image"
-                    />
-                    <div className="overlay">
-                        <p className="welcome-text">Welcome</p>
-                        <Link to="/library" className="cta-button">Go to your Bird Library</Link>
+            <div className="image-text-wrapper">
+
+                    <div className="top-text-box">
+                        <h2>Welcome to Birdr, your ultimate birdwatching companion.</h2>
+ 
+                    </div>
+                </div>
+                <div className="image-text-wrapper">
+                    <div className="image-wrapper rounded-image">
+                        <img src={treeOne} alt="Bird Image 1" className="large-image" />
+                    </div>
+                    <div className="text-area right-text">
+                        <h3>Avian Database Search</h3>
+                        <p>Search our database of over 1000 North American and European bird species. Powered by Nuthatch API's avian database, the Birdr
+                            database provides information including taxonomical classification, wingspan, conservation status and even includes images for
+                            select species. This tool is designed to make biredwatching more accessible and informative, helping you identify
+                            birds you may encounter.
+                        </p>
+                        <Link to="/search" className="home-button">Search for a Bird</Link>
+                    </div>
+                </div>
+                <div className="image-text-wrapper">
+                    <div className="text-area left-text">
+                    <h3>Manage your Avian Logbook</h3>
+                        <p>Explore your own personal bird logbook, where you can document your encounters with avian species and the stories behind them.
+                            Create new entries for your sightings and edit them at any time! Click below to access your comprehensive logbook and manage 
+                            your bird sightings.
+                        </p>
+                        <Link to="/library" className="home-button">Go to your Bird Library</Link>
+                    </div>
+                    <div className="image-wrapper rounded-image right-image">
+                        <img src={treeTwo} alt="Bird Image 2" className="large-image" />
                     </div>
                 </div>
             </div>
-            <p className="image-description">This is a description or additional information about the first bird.</p>
-        </div>
-    );
-};
 
-export default Home;
+
+        </div>
+    )
+}
+
+export default Home
