aec44a98dcede8eb340d01a49a4c89dd3382311e
Amanda Sumner <sumnera@oregonstate.edu>
2024-08-07 16:51:46 -0700

    improved styling of library page, added images

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -1,6 +1,7 @@
-import { useEffect} from 'react'
+import { useEffect } from 'react'
 import { useBirdsContext } from '../hooks/useBirdsContext'
 import { useAuthContext } from '../hooks/useAuthContext'
+import libraryImage from '../images/library.png'
 
 // components
 import BirdDetails from '../components/birdDetails'
@@ -30,8 +31,20 @@ const Library = () => {
     }, [dispatch, user])
 
     return (
-        <div className= "">
-            <BirdForm/>
+        <div>
+            <div className='library-header-container'>
+                <h3>Bird Log</h3>
+                <p>Create your own digital logbook for birds you have seen!</p>
+                <p>Add birds by entering bird name, location seen, your bird count for this species, and optionally enter any notes. You can delete entries for species you log at any time. Click update to change a species' count and edit notes.</p>
+            </div>
+            <a href="#logbook-header">
+                <img src={libraryImage} alt="Library Image" className="library-image" />
+            </a>
+            <BirdForm />
+            <div className='library-header-container' id="logbook-header">
+                <h3>Your Logbook</h3>
+                <p>Add or edit details of your encounters and observations.</p>
+            </div>
             <div className="birds">
                 {birds && birds.map((bird) => (
                     <BirdDetails key={bird._id} bird={bird}/>
